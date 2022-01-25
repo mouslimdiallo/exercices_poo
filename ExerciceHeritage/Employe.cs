686085ec2f2835f60df445cd1d9498135e0168ee
mouslimdiallo <dmuslim94@gmail.com>
2022-01-25 12:00:55 +0000

    commit1

---
+++ ExerciceHeritage/Employe.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceHeritage
+{
+    internal class Employe
+    {
+        protected string nom;
+        protected string fonction;
+        protected string email;
+        protected int bureau;
+
+    }
+
+    public static void Afficher()
+    {
+
+    }
+    static void Louer()
+    {
+
+    }
+    public Employe()
+    {
+        // Specifier les différents attributs à initialiser 
+
+    }
+    
+}
---
+++ ExerciceHeritage/Livre.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciceHeritage
+{
+    internal class Livre
+    {
+        public string titre;
+        public string auteur;
+        public string edition;
+        
+        public bool disponible;
+    }
+    public static void EstLouer()
+    {
+
+    }
+    public static Livre()
+    {
+
+    }
+}

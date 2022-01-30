278d6113ab942760c4ae7fcc2b93eeb51980b8a9
mouslimdiallo <dmuslim94@gmail.com>
2022-01-30 18:20:00 +0000

    marchandises

---
+++ Formation/ExoAbstractInterface/Marchandise.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marchandises
+{
+    public abstract Marchandise
+    {
+        double prix;
+        int poids;
+    }
+}
